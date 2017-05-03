6dbbce5044d94329ced47be816e4086b7e3f0760
SistemasCAE <sistemas_cae@cae3076.com>
2017-05-03 17:36:36 -0500

    pruebas modulacion

--- js/almacena.js
+++ js/almacena.js
@@ -7,7 +7,7 @@ var almacena = {
 		return window.openDatabase("Modulacion", "1.0", "Modulacion", 200000);
 	},
 	error: function(error){
-		if(error.message.indexOf("1555 UNIQUE constraint failed")!=-1);
+		if(error.message.indexOf("1555 UNIQUE constraint failed")>-1);
 		{
 			window.plugins.toast.show("El registro ya existe", 'long', 'center');
 			alert("El registro ya existe");
